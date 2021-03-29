d11ab9f28a2b3b319c4fef63e6b9cb09861ef74a
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-29 06:27:46 +0300

    Update test_parentheses.py

--- test_parentheses.py
+++ test_parentheses.py
@@ -1,13 +1,17 @@
 import parentheses
 
+"""
+Тестировариние программы правильности раположения скобок в предложении.
+"""
 class TestParent():
+
     def test_check_1(self):
         assert parentheses.check('>]]])(<>)(){}}]') == False        
     def test_check_2(self):
         assert parentheses.check('[{sgfdgs([[[<dfgsdfg>]]])(<>)(){}}]') == True      
     def test_check_3(self):
-        assert parentheses.check(']()(){<>}[[()]]') == False      
+        assert parentheses.check(']()sfsf(){<>}fs[[()fsdf]]') == False      
     def test_check_4(self):
         assert parentheses.check('{[][[]]]}') == True
-    def test_check_1(self):
-        assert parentheses.check('{{}{}}{{{{{}}}}}}}}{{}}}}') == False            
+    def test_check_5(self):
+        assert parentheses.check('{{}{}}{{{{{}ывфаыфаф}}}}}}}{{}}}}') == False   
